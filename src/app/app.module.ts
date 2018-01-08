dad7af6323d7ac90863e050ddcbccabc34aee92c
kumarz <2contactsathish@gmail.com>
2018-01-08 12:49:04 -0500

    Initial draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/constants.ts
@@ -0,0 +1,17 @@
+export const GET_USER_URL = '/tweeter/user/';
+export const GET_MESSAGES_URL = '/tweeter/messages/';
+export const POST_MESSAGE_URL = '/tweeter/message';
+export const CREATE_USER_URL = '/tweeter/user/';
+export const GET_CONECTIONS_USER_URL = '/tweeter/connections/';
+
+// tslint:disable-next-line:max-line-length
+export const CREATE_USER_REQUIRED_ERROR_MESSAGE = 'Please have userName, firstName and lastName entered inorder to create an user successfully.';
+export const CREATE_USER_SUCCESS_MESSAGE = 'User is created successfully.';
+export const CREATE_USER_FAILURE_MESSAGE = 'User is not created.';
+export const POST_MESSAGE_SUCCESS_MESSAGE = 'Message is posted successfully.';
+export const POST_MESSAGE_FAILURE_MESSAGE = 'Message is not posted.';
+export const USER_NOT_FOUND_MESSAGE = 'User not found, please have this user created.';
+
+export const SUCESS = 'success';
+export const FAILURE = 'failure';
+
